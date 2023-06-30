1651dfcfc9211ff6a8ed1c2619a1dbb9ee75d050
Eduardo Marques <edumqes@gmail.com>
2023-06-30 15:35:10 -0300

    Document Readme

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
 # Desafio_BigData_Minsait
 Avaliação do Treinamento Big Data Minsait
 
+Desenvolvimento de Projeto utilizando ferramentas de big data como Hive, Hadoop, Apache PySpark e Power BI considerando os arquivos abaixo disponibilizados em /input/desafio_curso/raw:
+
+    CLIENTES.csv
+    DIVISAO.csv
+    ENDERECO.csv
+    REGIAO.csv
+    VENDAS.csv
+
+# Para podermos iniciar as tratativas de desnvolvimento, se faz necessário executar o Docker para carga dos containers nos quais as ferramentas já estão diponibilizadas conforme arquivo docker-compose.yaml na pasta bigdata_docker
+
+1) Cópia automática dos arquivos .csv para hdfs dfs /datalake/raw/$tabela/$tabela.csv:
+ - Executada por shell script copy_files.sh em /input/desafio_curso/scripts/pre_process.
+
+2) Carga das tabelas no hive:
+ - Após criar database DESAFIO_CURSO, as tabelas foram carregadas utilizando script create_env_all.sh em /input/desafio_curso/scripts/pre_process.
+ - Este script irá executar os comandos "create table..." em scripts hql individuais para cada arquivo de carga, os mesmos constam em /input/desafio_curso/scripts/hql.
+ - Cada script .hql irá gerar duas tabelas sendo uma tabela externa e uma tabela gerenciada particionada para posterior processamento no PySpark (ex: tabela externa clientes e tabela gerenciada tbl_clientes com coluna adicional DTFOTO no qual constará a data em que os registros foram carregados).
+
+  - Observação: no script create_table_vendas.hql foi adicionada uma propriedade "serialization.null.format"='' para que os campos vazios no arquivo .csv fossem tratados como NULL na carga da tabela Hive.
+
+  - Abaixo seguem tabelas carregadas no Hive após execução dos scripts:
+    
+    Tabelas externas:
+        CLIENTES
+        DIVISAO
+        ENDERECO
+        REGIAO
+        VENDAS
+
+    Tabelas gerenciadas (com coluna adicional DTFOTO):
+        TBL_CLIENTES
+        TBL_DIVISAO
+        TBL_ENDERECO
+        TBL_REGIAO
+        TBL_VENDAS
+
+3) Criação dos dataframes para cada tabela hive
+ - Conforme solicitado no treinamento, os dataframes foram criados considerando todas as tabelas gerenciadas carregadas no hive utilizando o comando df_$tabela = spark.sql("select * from tbl_$tabela")
+ - Para as tabelas endereco e clientes, os campos que continham somente espaços foram carregados no data frame com valor 'Não Informado' conforme script process.py
+ - Para a tabela de vendas, devido à carga de campos nulos foi utilizado nvl(campo_null,0) para campos numéricos e nvl(Campo_null,'Não Informado') para campos do tipo string, para maiores detalhes consultar o script process.py em /input/desafio_curso/script/process
+ - Para carga da tabela stage foi utilizado comando join para relacionar todas as tabelas conforme modelo DER anexo em /input/desafio_curso/gold
+ - Para desenvolvimento sa dimensão tempo, foram adicionadas à tabela Stage as colunas Ano, Mês, Dia e Trimestre utilizando o campo invoice_date.
+ - As chaves para o modelo dimensional (DW_TEMPO, DW_LOCALIDADE e DW_CLIENTE) foram criadas com criptografia sha256 conforme consta no script process.py
+ - Após criação das chaves, os valores hash foram adicionados em novas colunas à tabela STAGE para posterior criação das dimensões conforme script pySpark.
+
+4) Criar dimensões e tabela fato
+ - Abaixo seguem dimensões geradas a partir das chaves gravadas na tabela Stage:
+
+        DIM_TEMPO -> Campos DW_TEMPO, invoice_date, stage.ano, stage.mes, stage.dia e stage.trimestre
+        DIM_LOCALIDADE -> Campos DW_LOCALIDADE, address_number, city, country, customer_address_1, customer_address_2, customer_address_3, customer_address_4, state e zip_code
+        DIM_CLIENTES -> Campos DW_CLIENTES, address_number, business_family, business_unit, customer, customerkey, customer_type, division, division_name, line_of_busines, phone, region_code, region_name, regional_sales_mgre e search_type.
+
+ - A tabela fato FT_VENDAS foi criada utilizando as três chaves já mencionadas anteriormente além dos campos invoice_date e sales_amount sumarizados "group by" considerando so demais critérios agrupados para que não hajam chaves duplicadas.
+
+ - As demais dimensões criadas não possuem campos duplicados conforme solicitado no treinemanto
+
+5) As tabelas dimensiomais e fato foram exportada para arquivos .csv disponibilizados em  /input/desafio_curso/gold, lembrando que ambos os arquivos relacionados abaixoque servirão de fonte para o dashboard Power BI:
+
+    DIM_CLIENTES.csv
+    DIM_LOCALIDADE.csv
+    DIM_TEMPO.csv
+    FT_VENDAS.csv
+
+6) Os arquivos mencionados acima foram consumidos pelo dashboard app Projeto Vendas.pbix no qual consta os seguintes indicadores:
+
+     - Total de vendas
+     - Quantidade de faturas
+     - Vendas por cliente (customerkey)
+     - Vendas por País
+     - Vendas por Ano e Mês
+
+Observação: valores podem ser exibidos de forma totalizada ou restrita (ex: vendas para um único cliente por ano/mês).
+
+
+
+
+
 
--- input/desafio_curso/scripts/process/process.py
+++ input/desafio_curso/scripts/process/process.py
@@ -1,3 +1,4 @@
+#Inport functions
 from pyspark.sql import SparkSession, dataframe
 from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import DoubleType, IntegerType, StringType
@@ -11,25 +12,197 @@ spark = SparkSession.builder.master("local[*]")\
     .enableHiveSupport()\
     .getOrCreate()
 
-# Criando dataframes diretamente do Hive
-df_clientes = spark.sql("SELECT * FROM DESAFIO_CURSO.TBL_CLIENTES")
+#Criar dataframes a partir do hive
+df_regiao = spark.sql("select * from desafio_curso.tbl_regiao")
+df_divisao = spark.sql("select * from desafio_curso.tbl_divisao")
 
-# Espaço para tratar e juntar os campos e a criação do modelo dimensional
+#Campos com espaço "Não Informado" - Clientes
+query_clientes = '''
+select address_number,business_family,business_unit,customer,customerkey,customer_type,division,
+case when length(trim(line_of_business)) = 0 then 'Não Informado' else line_of_business end as line_of_business,
+phone,region_code,regional_sales_mgr,search_type,dt_foto
+from desafio_curso.tbl_clientes'''
+df_clientes = spark.sql(query_clientes)
 
-# criando o fato
-ft_vendas = []
+#Campos com espaço "Não Informado" - Endereço
+query_endereco = '''
+select address_number,
+case when length(trim(city)) = 0 then 'Não Informado' else city end as city,country,
+case when length(trim(customer_address_1)) = 0 then 'Não Informado' else customer_address_1 end as customer_address_1,
+case when length(trim(customer_address_2)) = 0 then 'Não Informado' else customer_address_2 end as customer_address_2,
+case when length(trim(customer_address_3)) = 0 then 'Não Informado' else customer_address_3 end as customer_address_3,
+case when length(trim(customer_address_4)) = 0 then 'Não Informado' else customer_address_4 end as customer_address_4,
+case when length(trim(state)) = 0 then 'Não Informado' else state end as state,
+case when length(trim(zip_code)) = 0 then 'Não Informado' else zip_code end as zip_code,dt_foto
+from desafio_curso.tbl_endereco'''
+df_endereco = spark.sql(query_endereco)
 
-#criando as dimensões
-dim_clientes = []
+#Campos null como 'Não Informado' ou zero
+query_vendas = '''
+select actual_delivery_date,customerkey,datekey,
+nvl(replace(discount_amount,',','.'),0) as discount_amount,
+invoice_date,invoice_number,
+nvl(item_class,'Não Informado') as item_class,
+nvl(item_number,0) as item_number,
+item,line_number,
+replace(list_price,',','.') as list_price,
+order_number,promise_delivery_date,
+replace(sales_amount,',','.') as sales_amount,
+replace(sales_amount_based_on_list_price,',','.') as sales_amount_based_on_list_price,
+replace(sales_cost_amount,',','.') as sales_cost_amount,
+replace(sales_margin_amount,',','.') as sales_margin_amount,
+nvl(replace(sales_price,',','.'),0) as sales_price,
+sales_quantity,sales_rep,u_m
+from desafio_curso.tbl_vendas'''
+df_vendas = spark.sql(query_vendas)
 
-# função para salvar os dados
+#Views a partir dos dataframes gerados
+df_clientes.createOrReplaceTempView('clientes')
+df_regiao.createOrReplaceTempView('regiao')
+df_divisao.createOrReplaceTempView('divisao')
+df_endereco.createOrReplaceTempView('endereco')
+df_vendas.createOrReplaceTempView('vendas')
+
+#Tabela Stage
+query_stage = '''
+select
+    c.address_number,
+    c.business_family,
+    c.business_unit,
+    c.customer,
+    c.customerkey,
+    c.customer_type,
+    c.division,
+    c.line_of_business,
+    c.phone,
+    c.region_code,
+    c.regional_sales_mgr,
+    c.search_type,
+    d.division_name,
+    r.region_name,
+    e.city,
+    e.country,
+    e.customer_address_1,
+    e.customer_address_2,
+    e.customer_address_3,
+    e.customer_address_4,
+    e.state,
+    e.zip_code,
+    v.actual_delivery_date,
+    v.datekey,
+    v.discount_amount,
+    v.invoice_date,
+    v.invoice_number,
+    v.item_class,
+    v.item_number,
+    v.item,
+    v.line_number,
+    v.list_price,
+    v.order_number,
+    v.promise_delivery_date,
+    v.sales_amount,
+    v.sales_amount_based_on_list_price,
+    v.sales_cost_amount,
+    v.sales_margin_amount,
+    v.sales_price,
+    v.sales_quantity,
+    v.sales_rep,
+    v.u_m
+from vendas v
+inner join clientes c on c.customerkey = v.customerkey
+inner join divisao d on c.division = d.division
+inner join regiao r on c.region_code = r.region_code
+left join endereco e on c.address_number = e.address_number
+'''
+df_stage = spark.sql(query_stage)
+
+#Adicionar medidas de tempo (Ano, Mês, Dia, Trimestre) considerando invoice_date
+df_stage = (df_stage
+            .withColumn('Ano',f.year(f.to_timestamp('invoice_date','dd/MM/yyyy')))
+            .withColumn('Mes',f.month(f.to_timestamp('invoice_date','dd/MM/yyyy')))
+            .withColumn('Dia',f.dayofmonth(f.to_timestamp('invoice_date','dd/MM/yyyy')))
+            .withColumn('Trimestre',f.quarter(f.to_timestamp('invoice_date','dd/MM/yyyy')))
+           )
+
+#Criação de chaves para modelo dimensional
+df_stage = df_stage.withColumn("DW_LOCALIDADE", sha2(concat_ws("", df_stage.address_number,df_stage.city,df_stage.country,df_stage.customer_address_1,df_stage.customer_address_2,df_stage.customer_address_3,df_stage.customer_address_4,df_stage.state,df_stage.zip_code), 256))
+df_stage = df_stage.withColumn("DW_CLIENTES", sha2(concat_ws("", df_stage.address_number,df_stage.business_family,df_stage.business_unit,df_stage.customer,df_stage.customerkey,df_stage.customer_type,df_stage.division,df_stage.division_name,df_stage.line_of_business,df_stage.phone,df_stage.region_code,df_stage.region_name,df_stage.regional_sales_mgr,df_stage.search_type), 256))
+df_stage = df_stage.withColumn("DW_TEMPO", sha2(concat_ws("", df_stage.invoice_date,df_stage.Ano,df_stage.Mes,df_stage.Dia,df_stage.Trimestre), 256))
+
+df_stage.createOrReplaceTempView('stage')
+
+#Criando a dimensão Localidade (campos nulos tratados como 'Não Informado')
+dim_localidade = spark.sql('''
+    SELECT DISTINCT
+        DW_LOCALIDADE,
+        address_number,
+        nvl(city,'Não Informado') as city,
+        nvl(country,'Não Informado') as country,
+        nvl(customer_address_1,'Não Informado') as customer_address_1,
+        nvl(customer_address_2,'Não Informado') as customer_address_2,
+        nvl(customer_address_3,'Não Informado') as customer_address_3,
+        nvl(customer_address_4,'Não Informado') as customer_address_4,
+        nvl(state,'Não Informado') as state,
+        nvl(zip_code,'Não Informado') as zip_code
+    FROM stage    
+''')
+
+#Criando a dimensão Tempo
+dim_tempo = spark.sql('''
+    SELECT DISTINCT
+        DW_TEMPO,
+        invoice_date,
+        ano,
+        mes,
+        dia,
+        trimestre
+    FROM stage    
+''')
+
+#Criando a dimensão Cliente
+dim_cliente = spark.sql('''
+    SELECT DISTINCT
+        DW_CLIENTES,
+        address_number,
+        business_family,
+        business_unit,
+        customer,
+        customerkey,
+        customer_type,
+        division,
+        division_name,
+        line_of_business,
+        phone,
+        region_code,
+        region_name,
+        regional_sales_mgr,
+        search_type
+    FROM stage    
+''')
+
+#Criando tabela fato Vendas
+ft_vendas = spark.sql('''
+    SELECT 
+        DW_CLIENTES,
+        DW_LOCALIDADE,
+        DW_TEMPO,
+        invoice_number,
+        sum(sales_amount) as vl_total
+    FROM stage
+    group by 
+        DW_CLIENTES,
+        DW_LOCALIDADE,
+        DW_TEMPO,
+        invoice_number
+''')
+
+# Função para salvar os dados
 def salvar_df(df, file):
-    output = "/input/desafio_hive/gold/" + file
+    output = "/input/desafio_curso/gold/" + file
     erase = "hdfs dfs -rm " + output + "/*"
-    rename = "hdfs dfs -get /datalake/gold/"+file+"/part-* /input/desafio_hive/gold/"+file+".csv"
+    rename = "hdfs dfs -get /datalake/gold/"+file+"/part-* /input/desafio_curso/gold/"+file+".csv"
     print(rename)
-    
-    
+
     df.coalesce(1).write\
         .format("csv")\
         .option("header", True)\
@@ -40,4 +213,8 @@ def salvar_df(df, file):
     os.system(erase)
     os.system(rename)
 
-salvar_df(dim_clientes, 'dimclientes')
+# Salvando os dados em input/desafio_curso/gold
+salvar_df(ft_vendas, 'ft_vendas')
+salvar_df(dim_cliente, 'dim_cliente')
+salvar_df(dim_tempo, 'dim_tempo')
+salvar_df(dim_localidade, 'dim_localidade')
